5d7f74ba06390b8106d1d33bd230bbdee38c7dda
Alexius DIAKOGIANNIS <alexius.diakogiannis@gmail.com>
2019-10-14 21:59:51 +0300

    started routing

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {BugsDisplayComponent} from '../features/bug-management/components/bugs-display/bugs-display.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', component: BugsDisplayComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
