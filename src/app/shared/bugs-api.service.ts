0c1dd00d2ebe68846efd650c03c63c19f1a2cda3
OrestisKariotis <okariotis@outlook.com.gr>
2019-11-02 16:21:08 +0200

    corrected reporters-list

--- src/app/shared/bugs-api.service.ts
+++ src/app/shared/bugs-api.service.ts
@@ -14,7 +14,7 @@ export class BugsApiService {
 
   // Common Variables (that we should actually get from our backend):
   public priorities = [1, 2, 3];
-  public reporters = ['QA', 'DEV', 'TEST'];
+  public reporters = ['QA', 'DEV', 'PO'];
 
   constructor(private http: HttpClient) {
   }
